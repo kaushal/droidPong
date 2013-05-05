ab7de5fc4cb9ba3540bcf8deeee37d89c412911e
Sam Agnew <sagnew@eden.rutgers.edu>
2013-05-05 18:32:59 -0400

    Cleaned a little more

--- src/gamelogic/SinglePlayerLogic.java
+++ src/gamelogic/SinglePlayerLogic.java
@@ -8,47 +8,45 @@ import android.view.MotionEvent;
 
 public class SinglePlayerLogic {
 	// top and bottom of both paddles
-	// screen height and width 
+	// screen height and width
 	// size of the ball for shenanigans
 	// ball velocity in x and y
-	
+
 	int ballSize = (4 - com.example.droidpong.MainActivity.level) * 10;
-	
+
 	int heightOfScreen = 500;
 	int widthOfScreen = 840;
 	int topScore = 0;
 	int bottomScore = 0;
-	int ballx = 250; 
+	int ballx = 250;
 	int bally = 250;
-	
-	
+
+
 	int ballXVelocity = 3;//(int)Math.sin((Math.random() * 6.2));
 	int ballYVelocity = 3;//(int)Math.sin((Math.random() * 6.2));
-	
+
 	int paddleLength = 75;
 	int paddleWidth = 10;
-	
-	int topPaddleX = (widthOfScreen / 2) - (paddleLength /2); 
+
+	int topPaddleX = (widthOfScreen / 2) - (paddleLength /2);
 	int bottomPaddleX = topPaddleX;
-	
+
 	int topPaddleY = 100;
 	int bottomPaddleY = 740;
-	
+
 	int bottomOfTopPaddle = 105;
 	int topOfBottomPaddle = 735;
-	
-	
+
+
 	public void update() {
 		ballx = ballx + ballXVelocity;
 		bally = bally + ballYVelocity;
-		
+
 		if(ballx > topPaddleX && topPaddleX <= heightOfScreen)
 			topPaddleX += (com.example.droidpong.MainActivity.level);
 		else if(ballx < topPaddleX && topPaddleX >= 0)
 			topPaddleX -= (com.example.droidpong.MainActivity.level);
-		
-		
-		
+
 		if((bally + ballSize/2) < 95){ //Loosing
 			bottomScore += 1;
 			ballx = 250;
@@ -61,11 +59,11 @@ public class SinglePlayerLogic {
 			bally = 250;
 			ballYVelocity *= -1;
 		}
-			
+
 		else if((ballx + ballSize/2) >= heightOfScreen|| (ballx - ballSize/2) <= 0){
 			ballXVelocity *= -1;
 		}
-		
+
 		else if((bally - ballSize/2) <= 105 && (ballx + ballSize/2) <= (topPaddleX + paddleLength/2) && (ballx - ballSize/2) >= (topPaddleX - paddleLength/2)){
 			ballYVelocity *= -1;
 		}
@@ -73,32 +71,27 @@ public class SinglePlayerLogic {
 			ballYVelocity *= -1;
 			System.out.println(bally);
 		}
-		
-		
-		//top of the ball connecting wiht the bottom of the top paddle
-		
-		
-		
+
 	}
-	
+
 	public boolean motionDetected(MotionEvent event) {
 		bottomPaddleX = (int) event.getX() - paddleLength/2;
 		return true;
 	}
-	
+
 	public void draw(Canvas canvas, Paint paint) {
 		canvas.drawRGB(20, 20, 20);
 		paint.setARGB(200, 255, 255,255);
-		
+
 		canvas.drawRect(new Rect(ballx - ballSize/2,bally + ballSize/2,ballx + ballSize/2,bally - ballSize/2),paint);
-		
+
 		canvas.drawRect(new Rect(topPaddleX - paddleLength/2, topPaddleY + paddleWidth/2, topPaddleX + paddleLength/2, topPaddleY - paddleWidth/2), paint); //top Paddle
 		canvas.drawRect(new Rect(bottomPaddleX - paddleLength/2, bottomPaddleY + paddleWidth/2, bottomPaddleX + paddleLength/2, bottomPaddleY - paddleWidth/2), paint); //bot
 		paint.setStrokeWidth(4);
 		Paint newPaint = new Paint();
 		newPaint.setARGB(200, 255, 255,255);
-		
-		
+
+
 		newPaint.setTextSize(50);
 		canvas.drawText(topScore + "", 450, 410, newPaint);
 		canvas.drawText(bottomScore + "", 450, 465, newPaint);
@@ -108,6 +101,5 @@ public class SinglePlayerLogic {
 		canvas.drawLine(0, 840, 500, 840, paint);
 		canvas.drawLine(500, 0, 500, 840, paint);
 	}
-	
 
 }
